e1c80c2d3b025a9ff63ff6e1533dec3864fbb630
scarf <greenscarf005@gmail.com>
2023-03-14 16:11:22 +0900

    style: fmt

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { App } from '@slack/bolt'
-import { config as loadDotenv } from 'dotenv'
+import { App } from "@slack/bolt"
+import { config as loadDotenv } from "dotenv"
 
 /**
  * `.env` 파일 내의 환경 변수 타입 정의
@@ -8,37 +8,38 @@ import { config as loadDotenv } from 'dotenv'
  * @note 값 string
  */
 type DotEnv = Record<
-  'SLACK_BOT_TOKEN' | 'SLACK_SIGNING_SECRET' | 'SLACK_APP_TOKEN' | 'PORT',
-  string
+	"SLACK_BOT_TOKEN" | "SLACK_SIGNING_SECRET" | "SLACK_APP_TOKEN" | "PORT",
+	string
 >
 
+console.log("⚡️ started script")
 const { SLACK_APP_TOKEN, SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET, PORT } =
-  loadDotenv().parsed as DotEnv
+	loadDotenv().parsed as DotEnv
 
 const port = parseInt(PORT, 10)
-
 const app = new App({
-  token: SLACK_BOT_TOKEN,
-  signingSecret: SLACK_SIGNING_SECRET,
-  socketMode: true,
-  appToken: SLACK_APP_TOKEN,
-  port,
+	token: SLACK_BOT_TOKEN,
+	signingSecret: SLACK_SIGNING_SECRET,
+	socketMode: true,
+	appToken: SLACK_APP_TOKEN,
+	port,
 })
 
 // 사용자의 메시지 안에 "hello" 문자열이 포함되어 있으면 실행
-app.message('hello', async ({ message, say }) => {
-  if (message.subtype) return // 타입스크립트 오류 방지를 위해 추가
+app.message("hello", async ({ message, say }) => {
+	if (message.subtype) return // 타입스크립트 오류 방지를 위해 추가
 
-  console.log(`Message received: ${message.text}`)
+	console.log(`Message received: ${message.text}`)
 
-  // <>는 슬랙이 쓰는 변수 표기법, js의 `${값}` 과 비슷함
-  await say(`Hey there <@${message.user}>!`)
+	// <>는 슬랙이 쓰는 변수 표기법, js의 `${값}` 과 비슷함
+	await say(`Hey there <@${message.user}>!`)
 })
 
 const main = async () => {
-  await app.start(port)
+	console.log("⚡️ Starting Bolt app...")
+	await app.start(port)
 
-  console.log('⚡️ Bolt app is running!')
+	console.log("⚡️ Bolt app is running!")
 }
 
 main()
