468196cd14316ef66de107599a05d61ffd65843c
scarf <greenscarf005@gmail.com>
2023-03-16 11:00:34 +0900

    refactor: hello 훅을 createHook을 쓰게 변경

--- src/hooks/hello.ts
+++ src/hooks/hello.ts
@@ -1,6 +1,5 @@
-import { MessageHook } from "../app"
+import { createHook } from "../createHook"
 
-// regex to match 안녕 or hello
 const trigger = /안녕|반가워|hello/i
 
 /**
@@ -9,14 +8,12 @@ const trigger = /안녕|반가워|hello/i
  * 사용자 메시지 중 `안녕, 반가워, hello` 중 하나가 포함되면
  * `안녕안녕 {사용자이름}!` 이라고 답장합니다
  */
-export const helloHook: MessageHook = {
-	message: trigger,
-	fn: async ({ message, say, ...args }) => {
-		if (message.subtype) return // 타입스크립트 오류 방지를 위해 추가
-
+export const helloHook = createHook({
+	trigger,
+	fn: async ({ message, say }) => {
 		console.log(`Message received: ${message.text}`)
 
 		// <>는 슬랙이 쓰는 변수 표기법, js의 `${값}` 과 비슷함
 		await say(`안녕안녕 ${message.user} <@${message.user}>!`)
 	},
-}
+})
