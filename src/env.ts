2c19534366ac7c78d1892a59001d4160c10a4513
scarf <greenscarf005@gmail.com>
2023-03-16 11:42:01 +0900

    refactor: `.env` 파일 검증

--- src/env.ts
+++ src/env.ts
@@ -1,32 +1,38 @@
 import { config as loadDotenv } from "dotenv"
+import { z } from "zod"
 
-/**
- * `.env` 파일 내의 환경 변수 타입 정의
- *
- * @note 키 `SLACK_BOT_TOKEN`, `SLACK_SIGNING_SECRET`, `SLACK_APP_TOKEN`, `PORT`
- * @note 값 string
- */
-type EnvFileEntry = Record<
-	| "SLACK_BOT_TOKEN"
-	| "SLACK_SIGNING_SECRET"
-	| "SLACK_APP_TOKEN"
-	| "OPENAI_API_KEY",
-	string
->
+type EnvFileEntry = z.infer<typeof envFileSchema>
+
+/** .env 파일 내에 반드시 있어야 하는 키값 목록 */
+const envFileSchema = z
+	.object({
+		SLACK_BOT_TOKEN: z.string(),
+		SLACK_SIGNING_SECRET: z.string(),
+		SLACK_APP_TOKEN: z.string(),
+		OPENAI_API_KEY: z.string(),
+		PORT: z.string(),
+	})
+	.strict()
 
 /** 환경 변수 파일 */
-type EnvFile = EnvFileEntry & { PORT: string }
+type EnvFile = EnvFileEntry
 
 /** 파싱이 끝난 환경 변수 객체 */
-export type Env = Readonly<EnvFileEntry & { port: number }>
+export type Env = Readonly<Omit<EnvFileEntry, "PORT"> & { port: number }>
 
-const envfile = loadDotenv().parsed as EnvFile
-
+/** 포트 번호를 숫자로 파싱합니다 */
 const parseEnv = ({ PORT, ...args }: EnvFile): Env => ({
 	...args,
 	port: parseInt(PORT, 10),
 })
 
-export const env = parseEnv(envfile)
-export const { SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET, SLACK_APP_TOKEN, port } =
-	env
+const envfile = loadDotenv().parsed as EnvFile
+
+export const env = parseEnv(envFileSchema.parse(envfile))
+export const {
+	SLACK_BOT_TOKEN,
+	SLACK_SIGNING_SECRET,
+	SLACK_APP_TOKEN,
+	OPENAI_API_KEY,
+	port,
+} = env
