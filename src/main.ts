6a17543491b5dd2ce4e729b53fef70f112169b3f
scarf <greenscarf005@gmail.com>
2023-03-16 11:00:16 +0900

    feat: echo hook 예제 추가

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { env, port } from "./env"
 import { createApp, registerHooks } from "./app"
+import { echoHook } from "./hooks"
 
 const main = async () => {
 	console.log("⚡️ 앱 실행 준비 중...")
+
 	const app = createApp(env)
+	registerHooks(app, [echoHook])
 	await app.start(port)
 
-	console.log("⚡️ Bolt app is running!")
+	console.log("⚡️ 앱 시작!")
 }
 
 main()
