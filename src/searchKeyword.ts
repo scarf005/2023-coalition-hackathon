212d10c143644fa5471e4e91ab4f4024bab67f2d
stsr1284 <1599zzang@gmail.com>
2023-03-18 17:55:03 +0900

    feat: 키워드 검색시 대소문자 구별하지 않게 변경

--- src/searchKeyword.ts
+++ src/searchKeyword.ts
@@ -8,10 +8,14 @@ export const docSchema = z.object({
 export type Doc = z.infer<typeof docSchema>
 
 /**
- * 주어진 {@link Doc} 배열에서 `keyword`를 찾아서 `contents`를 반환합니다.
+ * 주어진 {@link Doc} 배열에서 `keyword`를 찾아서 `내용`을 반환합니다.
  */
 export const searchDocKeyword = (
-	keyword: string,
 	docs: Doc[],
-): Doc["내용"] | null =>
-	docs.find(({ 키워드 }) => 키워드.includes(keyword))?.내용 ?? null
+	keyword: string,
+): Doc["내용"] | null => {
+	const lowerKeyword = keyword.toLowerCase()
+	const result = docs.find(({ 키워드 }) => 키워드.includes(lowerKeyword))
+
+	return result?.내용 ?? null
+}
