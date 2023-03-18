c324f32531f9cf5c7101bbac5f3f17ebd70dfa48
stsr1284 <68745729+stsr1284@users.noreply.github.com>
2023-03-18 14:16:46 +0900

    Update src/hooks/search_keyword_hook.ts
    Co-authored-by: scarf <greenscarf005@gmail.com>

--- src/hooks/search_keyword_hook.ts
+++ src/hooks/search_keyword_hook.ts
@@ -3,17 +3,11 @@ type Doc = {
     contents: string[]
 }
 
-const getTextInDoc = async (
-    keyword: string,
-    document: Doc[]
-    ): Promise<string[]> => {
-      const matchingDocs = document.filter((Doc) => Doc.keywords.includes(keyword));
-      const contentPromises = matchingDocs.map(async (Doc: Doc) => Doc.contents.join(" "));
-
-      return  await Promise.all(contentPromises);
-    }
-
-
-  export const searchDocKeyword = async (keywrod: string, documentList:Doc[]): Promise<string> => {
-      return (await getTextInDoc(keywrod, documentList)).join(" ")
-  }
+/**
+ * 주어진 {@link Doc} 배열에서 `keyword`를 찾아서 `contents`를 반환합니다.
+ */
+export const searchDocKeyword = (
+	keyword: string,
+	docs: Doc[],
+): Doc["contents"] | null =>
+	docs.find(({ keywords }) => keywords.includes(keyword))?.contents ?? null
