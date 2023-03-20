08f043059bc29c0e2d20893a902ee5e90dc3d5ae
scarf <greenscarf005@gmail.com>
2023-03-20 14:00:02 +0900

    fix: process.env 사용하게 변경
    `.env`파일이 없어도 환경 변수만으로 실행할 수 있게

--- src/env.ts
+++ src/env.ts
@@ -2,20 +2,18 @@ import { config as loadDotenv } from "dotenv"
 import { z } from "zod"
 
 /** .env 파일 내에 반드시 있어야 하는 키값 목록 */
-const envFileSchema = z
-	.object({
-		SLACK_BOT_TOKEN: z.string(),
-		SLACK_SIGNING_SECRET: z.string(),
-		SLACK_APP_TOKEN: z.string(),
-		OPENAI_API_KEY: z.string().optional(),
-		ANNOUNCEMENT_CHANNEL_ID: z.string(),
-		PORT: z.coerce.number(),
-	})
-	.strict()
+const envFileSchema = z.object({
+	SLACK_BOT_TOKEN: z.string(),
+	SLACK_SIGNING_SECRET: z.string(),
+	SLACK_APP_TOKEN: z.string(),
+	OPENAI_API_KEY: z.string().optional(),
+	ANNOUNCEMENT_CHANNEL_ID: z.string(),
+	PORT: z.coerce.number(),
+})
 
-const envfile = loadDotenv().parsed
+loadDotenv()
 
-export const env = envFileSchema.parse(envfile)
+export const env = envFileSchema.parse(process.env)
 export const {
 	SLACK_BOT_TOKEN,
 	SLACK_SIGNING_SECRET,
