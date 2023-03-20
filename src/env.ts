51803624d29f760baf8b0023213c7fb58586c672
scarf <greenscarf005@gmail.com>
2023-03-20 11:33:44 +0900

    refactor: zod coerce 사용 (#30)
    coerce 기능을 사용해 코드 간략화
    참고: https://zod.dev/?id=coercion-for-primitives

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,6 @@
 import { config as loadDotenv } from "dotenv"
 import { z } from "zod"
 
-type EnvFileEntry = z.infer<typeof envFileSchema>
-
 /** .env 파일 내에 반드시 있어야 하는 키값 목록 */
 const envFileSchema = z
 	.object({
@@ -11,30 +9,18 @@ const envFileSchema = z
 		SLACK_APP_TOKEN: z.string(),
 		OPENAI_API_KEY: z.string().optional(),
 		ANNOUNCEMENT_CHANNEL_ID: z.string(),
-		PORT: z.string(),
+		PORT: z.coerce.number(),
 	})
 	.strict()
 
-/** 환경 변수 파일 */
-type EnvFile = EnvFileEntry
+const envfile = loadDotenv().parsed
 
-/** 파싱이 끝난 환경 변수 객체 */
-export type Env = Readonly<Omit<EnvFileEntry, "PORT"> & { port: number }>
-
-/** 포트 번호를 숫자로 파싱합니다 */
-const parseEnv = ({ PORT, ...args }: EnvFile): Env => ({
-	...args,
-	port: parseInt(PORT, 10),
-})
-
-const envfile = loadDotenv().parsed as EnvFile
-
-export const env = parseEnv(envFileSchema.parse(envfile))
+export const env = envFileSchema.parse(envfile)
 export const {
 	SLACK_BOT_TOKEN,
 	SLACK_SIGNING_SECRET,
 	SLACK_APP_TOKEN,
 	OPENAI_API_KEY,
 	ANNOUNCEMENT_CHANNEL_ID,
-	port,
+	PORT,
 } = env
